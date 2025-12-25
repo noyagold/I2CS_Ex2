b4d0ac6e8dc343a684eff8f8fdadf753cedb3324
noyagold <noyagold7@gmail.com>
2025-12-25 10:49:28 +0200

    added "this" to x and y

--- src/assignments/Ex2/Index2D.java
+++ src/assignments/Ex2/Index2D.java
@@ -4,24 +4,24 @@ public class Index2D implements Pixel2D {
     private int x;
     private int y;
     public Index2D(int w, int h) {
-        x = w;
-        y = h;
+       this.x = w;
+        this.y = h;
     }
 
     public Index2D(Pixel2D other) {
-         x = other.getX();
-         y=other.getY();
+         this.x = other.getX();
+         this.y=other.getY();
     }
     @Override
     public int getX() {
 
-        return x;
+        return this.x;
     }
 
     @Override
     public int getY() {
 
-        return y;
+        return this.y;
     }
 
     @Override
@@ -35,7 +35,7 @@ public class Index2D implements Pixel2D {
 
     @Override
     public String toString() {
-         String ans = "(" + x + ","+ y + ")";
+         String ans = "(" + this.x + ","+ this.y + ")";
         return ans;
     }
 
@@ -45,7 +45,7 @@ public class Index2D implements Pixel2D {
         if (p==null){return ans;}
         if (p instanceof Index2D ) {        //checks if p is from class Index2D
             Index2D pp = (Index2D) p;       // looks at p as an object from Index2D  (casting)
-           if (pp.getX()==x && pp.getY()==y) {   //object.method
+           if (pp.getX()==this.x && pp.getY()==this.y) {   //object.method
                ans = true;
            }
         }
