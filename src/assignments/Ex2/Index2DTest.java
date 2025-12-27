09fc069358d50fcaf0027401bcc5db1954309303
noyagold <noyagold7@gmail.com>
2025-12-28 01:38:04 +0200

    test for distances and a setup

--- src/assignments/Ex2/Index2DTest.java
+++ src/assignments/Ex2/Index2DTest.java
@@ -1,9 +1,16 @@
 package assignments.Ex2;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Index2DTest {
+    private static Index2D p;
+
+    @BeforeAll
+    static void setup() {
+        p = new Index2D(0, 0);
+    }
 
     @Test
     //x negative
@@ -34,6 +41,29 @@ public class Index2DTest {
         assertEquals(5.0, p1.distance2D(p2));
     }
 
+    @Test
+//  null
+    public void testDistance2DNull() {
+        try {
+            p.distance2D(null);
+            fail(); // must throw exception
+        } catch (RuntimeException e) {
+        }
+    }
+
+    // distance on Y axis
+    @Test
+    void testDistance2D_Y() {
+        Index2D a = new Index2D(0, 4);
+        assertEquals(4.0, p.distance2D(a));
+    }
+
+    // distance diagonal
+    @Test
+    void testDistance2D_Diagonal() {
+        Index2D a = new Index2D(3, 4);
+        assertEquals(5.0, p.distance2D(a), 0.001);
+    }
 
 
 
